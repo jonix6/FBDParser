665b98b199851e98578c66ec27e79b8b8d79b091
jonix6 <jonix.lau@gmail.com>
2021-11-03 14:32:54 +0800

    增加异常类型

---
+++ FBDParser/exceptions.py
@@ -0,0 +1,23 @@
+
+class FBDCommandError(Exception):
+    pass
+
+
+class FBDEntityError(Exception):
+    pass
+
+
+class FBDEOF(Exception):
+    pass
+
+
+class FBDNestingError(Exception):
+    pass
+
+
+class FBDTemplateError(Exception):
+    pass
+
+
+class FBDSyntaxError(Exception):
+    pass
