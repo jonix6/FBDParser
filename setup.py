d4ee08f253c7f94a4d50c907a3f3ca87bd36f311
jonix6 <jonix.lau@gmail.com>
2021-11-24 10:23:06 +0800

    项目打包配置

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(
+    name="FBDParser", 
+    version="0.1.0", 
+    url="https://github.com/jonix6/FBDParser", 
+    author="jonix6", 
+    author_email="jonix.lau@gmail.com", 
+    packages=[
+        "FBDParser", 
+        "FBDParser/patterns", 
+        "FBDParser/charmaps"
+    ], 
+    # zip_safe = False
+)
